dfee3dab34655ee9815b7026fbeffff40ef5fc83
MXMKGN <84654280+Maximekgn@users.noreply.github.com>
2024-03-10 12:17:15 +0000

    Add files via upload

---
+++ js.js
@@ -0,0 +1,17 @@
+const random = Math.floor(Math.random() * 10) + 1;
+const num = document.getElementById("number");
+const phrase = document.getElementById("phrase");
+const button = document.getElementById("button");
+
+button.onclick = function() {
+    let userNum = Number(num.value);
+    if(userNum > random){
+        phrase.textContent = "Votre nombre est trop élevé.";
+    }
+    else if (userNum < random) {
+        phrase.textContent = "Votre nombre est trop bas.";
+    }
+    else if (userNum == random ) {
+        phrase.textContent = "BINGO, vous avez trouvé!";
+    }
+}
