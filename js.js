5b0ceda6b2304505c1359d9ca19870a084836ee9
MXMKGN <kogonmaxime@gmail.com>
2024-03-22 11:56:35 +0000

    amelioration

--- js.js
+++ js.js
@@ -1,17 +1,35 @@
-const random = Math.floor(Math.random() * 100) + 1;
-const num = document.getElementById("number");
+const random = Math.floor(Math.random() * 100 + 1 ) 
 const phrase = document.getElementById("phrase");
 const button = document.getElementById("button");
 
-button.onclick = function() {
-    let userNum = Number(num.value);
-    if(userNum > random){
-        phrase.textContent = "Your number is too high";
+
+let attemps = 0 ;
+let running = true ;
+
+ 
+    button.onclick = function() {
+        var user_number = document.getElementById("number").value;
+        user_number = Number(user_number);
+        if (isNaN(user_number))
+        {
+            phrase.textContent = "PLEASE ENTER A CORRECT NUMBER";
+        }
+        else if (user_number<0 || user_number>100 )
+        {
+            phrase.textContent = " PLEASE ENTER A NUMBER BEETWEEN 0-100" ;
+        }
+        else 
+        {   
+            if(user_number > random){
+                phrase.textContent = "Your number is too high";
+            }
+            else if (user_number < random) {
+                phrase.textContent = "Your Number is too low ";
+            }
+            else if (user_number == random ) {
+                phrase.textContent = `BINGO, you win! the number was ${random} ; it took you ${attemps+1} attemps nigga !`;
+            }
+            attemps ++ ;
+        }
+       
     }
-    else if (userNum < random) {
-        phrase.textContent = "Your Number is too low ";
-    }
-    else if (userNum == random ) {
-        phrase.textContent = `BINGO, you win! the number was ${random}`;
-    }
-}
